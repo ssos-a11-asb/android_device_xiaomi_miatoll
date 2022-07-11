b0c5fe5eb270cc9a599ad57aaec88a1ccb11eef2
LeddaZ <leonardoledda@gmail.com>
2022-07-11 15:27:41 +0200

    miatoll: Inherit common_full_phone makefile

--- ssos_miatoll.mk
+++ ssos_miatoll.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ShapeShiftOS stuff.
-$(call inherit-product, vendor/ssos/config/common.mk)
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
